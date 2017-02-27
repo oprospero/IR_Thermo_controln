ff7b832391386de30473f1cb07d7f52982901b68
Prosper <oprospero@user.noreply.github.com>
2017-02-26 16:43:39 -0800

    Added Blynk library
    Added Wifi info

--- IRThermoControl/src/main.cpp
+++ IRThermoControl/src/main.cpp
@@ -1,8 +1,19 @@
 
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
+#include <ESP8266WiFi.h>
+#include "BlynkSimpleEsp8266.h"
 
 #define IR_LED_PIN 1
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "HouseOfMack_EXT";
+char pass[] = "SeanSarahMack123";
 
 IRsend IrOut(IR_LED_PIN);
 
