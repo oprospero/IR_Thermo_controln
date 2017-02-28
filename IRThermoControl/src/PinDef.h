90985ef31b8935b9cabfd56f13afefda6a210c00
Prosper <vanprosper@gmail.com>
2017-02-27 21:33:55 -0800

    Seperated defines into files. (Pins and signals)

---
+++ IRThermoControl/src/PinDef.h
@@ -0,0 +1,7 @@
+
+#ifndef PINDEF_H
+#define PINDEF_H
+
+#define IR_LED_PIN 1
+
+#endif
--- IRThermoControl/src/main.cpp
+++ IRThermoControl/src/main.cpp
@@ -1,10 +1,9 @@
 
 #include <Arduino.h>
-#include <IRremoteESP8266.h>
 #include <ESP8266WiFi.h>
+#include "IRremoteESP8266.h"
 #include "BlynkSimpleEsp8266.h"
 
-#define IR_LED_PIN 1
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -15,7 +14,7 @@ char auth[] = "3771a29413a54d7387eaae537228ed95";
 char ssid[] = "HouseOfMack_EXT";
 char pass[] = "SeanSarahMack123";
 
-IRsend IrOut(IR_LED_PIN);
+// IRsend IrOut(IR_LED_PIN);
 
 // BLYNK_CONNECTED() {
 //   if (isFirstConnect) {
@@ -26,7 +25,7 @@ IRsend IrOut(IR_LED_PIN);
 void setup()
 {
   Serial.begin(9600);
-  IrOut.begin();
+  // IrOut.begin();
   Blynk.begin(auth, ssid, pass);
 }
 
